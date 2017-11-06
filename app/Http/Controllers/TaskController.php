7eff1d5c33b0fd61e6034a6d847c991a4a9062c6
Viacheslav <vm.php.academy@gmail.com>
2017-11-06 10:23:12 +0200

    CRUD works

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::where(['user_id' => Auth::user()->id])->get();
+        return response()->json([
+            'tasks'    => $tasks,
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'        => 'required|max:255',
+            'description' => 'required',
+        ]);
+
+        $task = Task::create([
+            'name'        => request('name'),
+            'description' => request('description'),
+            'user_id'     => Auth::user()->id
+        ]);
+
+        return response()->json([
+            'task'    => $task,
+            'message' => 'Success'
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param Task $task
+     * @return \Illuminate\Http\Response
+     * @internal param int $id
+     */
+    public function update(Request $request, Task $task)
+    {
+        $this->validate($request, [
+            'name'        => 'required|max:255',
+            'description' => 'required',
+        ]);
+
+        $task->name = request('name');
+        $task->description = request('description');
+        $task->save();
+
+        return response()->json([
+            'message' => 'Task updated successfully!'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
